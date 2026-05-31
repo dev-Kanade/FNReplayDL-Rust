1464f3f2a2fabaa023dec5db1d192db04cfdcf0a
dev-kanade <kanade.miyachi@gmail.com>
2026-05-31 11:34:23 +0900

    使用するAPIなど

---
+++ rust/src/constants.rs
@@ -0,0 +1,11 @@
+pub const META_DATA_URL: &str = "https://datastorage-public-service-live.ol.epicgames.com/api/v1/data/fnreplaysmetadata/public/";
+pub const BASE_DATA_URL: &str =
+    "https://datastorage-public-service-live.ol.epicgames.com/api/v1/access/fnreplays/public/";
+pub const TOKEN_ENDPOINT: &str =
+    "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token";
+pub const VERIFY_ENDPOINT: &str =
+    "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/verify";
+
+pub const AUTH_CLIENT_ID: &str = "3f69e56c7649492c8cc29f1af08a8a12";
+pub const AUTH_CLIENT_SECRET: &str = "b51ee9cb12234f50a69efa67ef53812e";
+pub const USER_AGENT: &str = "fortnite-replay-downloader-rust";
